ffe130ee75c2de6d4d9e9a7c443661b98636cd52
Koki Kamezono <kow1ag25@gmail.com>
2021-01-16 19:19:51 +0900

    env-2 watchタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,24 @@ const sass = require("gulp-sass");
 
 // style.scssのタスクを作成する
 gulp.task("default", function () {
-  return(
-    gulp
-      .src("css/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(sass({
-        outputStyle: "expanded"
-      }))
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
-  );
+  // style.scssファイルを監視
+  return gulp.watch("css/style.scss", function(){
+    // style.scssの更新があった場合の処理
+
+    // style.scssファイルを取得
+    return (
+      gulp
+        .src("css/style.scss")
+        // Sassのコンパイルを実行
+        .pipe(
+          sass({
+            outputStyle: "expanded"
+          })
+          // Sassのコンパイルエラーを表示
+          .on("error", sass.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("css"))
+    );
+  });
 });
